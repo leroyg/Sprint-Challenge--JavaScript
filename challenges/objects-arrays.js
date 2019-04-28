6ca5a092b0784bcceb4d8e6c16f119b4aedd4726
leroyg <leroy.gardner@gmail.com>
2019-04-28 18:15:17 -0500

    added speak function

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,6 +12,10 @@ class Dinosaur {
         this.weight = attribute.weight;
         this.length = attribute.length;
         this.period = attribute.period;
+        this.newPhrase = attribute.phrase;    
+    }
+    speak() {
+        return `${this.newPhrase}`;
     };
 }
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
@@ -21,6 +25,7 @@ const trex = new Dinosaur({
     weight: `7000kg`,
     length: `12m`,
     period: `Late Cretaceous`,
+    phrase: `RAWERSRARARWERSARARARRRR!`,
 });
 console.log(trex);
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -45,20 +50,20 @@ console.log(velo);
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velo.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.speak());
 
 
 // ==== Arrays ====
